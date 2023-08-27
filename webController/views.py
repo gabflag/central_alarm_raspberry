a772e9d3cbadda03fc04dded3750f14c0dabadde
GabFlag <contact_rkg@protonmail.com>
2023-08-27 14:15:48 -0300

    Add pages

--- webController/views.py
+++ webController/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
+@login_required
+def dashboard(request):
+    return render(request, 'main/dashboard.html')
+
+def home(request):
+    return render(request, 'main/home.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('dashboard')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/singup.html', {'form': form})
